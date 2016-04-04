f1b41a2df1124d1d3deeb6195d89934d2aa4ed14
patsykakaz <philippe@lesidecar.fr>
2016-04-04 19:13:49 +0200

    custom tags added

--- USERMGMT/urls.py
+++ USERMGMT/urls.py
@@ -3,14 +3,15 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+from . import views
 
 
 urlpatterns = patterns('',
-    url(r'login/$', 'USERMGMT.views.connect', name='connect'),
-    url(r'logout/$', 'USERMGMT.views.killUser', name='killUser'),
-    url(r'abonnement/$', 'USERMGMT.views.ask_abo', name='ask_abo'),
-    url(r'display/$', 'USERMGMT.views.showUser', name='showUser'),
-    url(r'modification/$', 'USERMGMT.views.changeUser', name='changeUser'),
-    url(r'newPassword/$', 'USERMGMT.views.newPassword', name='newPassword'),
-    url(r'forgottenPassword/$', 'USERMGMT.views.forgottenPassword', name='forgottenPassword'),
+    url(r'login/$', views.connect, name='connect'),
+    url(r'logout/$', views.killUser, name='killUser'),
+    url(r'abonnement/$', views.ask_abo, name='ask_abo'),
+    url(r'display/$', views.showUser, name='showUser'),
+    url(r'modification/$', views.changeUser, name='changeUser'),
+    url(r'newPassword/$', views.newPassword, name='newPassword'),
+    url(r'forgottenPassword/$', views.forgottenPassword, name='forgottenPassword'),
 )
